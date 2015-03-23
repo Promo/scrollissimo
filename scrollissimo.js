38196d7e98a3a11263284f24251bdaa5598ff4c4
Vladimir Kudinov <frux@yandex-team.ru>
2015-03-23 16:14:33 +0500

    Reworked

--- scrollissimo.js
+++ scrollissimo.js
@@ -26,6 +26,7 @@
     }
 }());
 
+
 /**
  * Scrollissimo initialization
  * @param callback {Function} Initialization end's callback
@@ -33,12 +34,14 @@
  * @Singletone
  */
 function Scrollissimo(callback){
-    var S = {}, //S is for singletone :)
-        docHeight = getDocumentHeight() || 0,
-        windowHeight = Number(window.innerHeight) || window.innerHeight,
+    var S = {}, //S is for Scrollissimo
+        lastScroll = 0, //to check if document was scrolled
+        docHeight = getDocumentHeight() || 0, //document height
+        windowHeight = Number(window.innerHeight) || window.innerHeight, //window height
         smoothQueues = [], //all animations with smooth effect
         queues = [], //all other animations queues
-        lastScroll = 0; //TODO: Remove this
+        getScrollTop,
+        setCSSProperty;
 
     /**
      * Calculate document's height
@@ -57,8 +60,28 @@ function Scrollissimo(callback){
      * @returns {Number} Length of scrolled area in pixels
      */
     function getScrollTop(){
-        return (window.pageYOffset || document.scrollTop || 0) - (document.clientTop || 0);
+        if('pageYOffset' in window){
+            getScrollTop = function(){
+                return (window.pageYOffset || 0) - (document.clientTop || 0);
+            };
+        }else{
+            getScrollTop = function(){
+                return (document.scrollTop || 0) - (document.clientTop || 0);
+            };
+        }
+        return getScrollTop();
     }
+
+    if(window.jQuery){
+        setCSSProperty = function(target, property, value){
+            $(target).css(property, value);
+        }
+    }else{
+        setCSSProperty = function(target, property, value){
+            target.style.setProperty(property, value);
+        }
+    }
+
 
     /**
      * Converts pixels in percents related to a document's height
@@ -66,36 +89,114 @@ function Scrollissimo(callback){
      * @depends getDocumentHeight
      * @returns {Number} Value in percents
      */
-    function px2percents(px){
-        return parseInt(px) / getDocumentHeight();
+    function toPercents(px, documentHeight){
+        documentHeight = documentHeight || docHeight;
+
+        //if
+        if(typeof px === 'string'){
+            if(px.substr(-1, 1) === '%'){
+                return parseInt(px) / 100;
+            }else if(px.substr(-2, 2) === 'px'){
+                return (parseInt(px) / documentHeight);
+            }
+            return parseInt(px) / getDocumentHeight();
+        }else if(!isNaN(px)){
+            return (px / documentHeight);
+        }
     }
 
     /**
      * Configure render function for animating specified property
      * @param target {HTMLElement} Target of animation
-     * @param params {Object} Object contains animation params
-     * @returns {Function} Animation render function
+     * @param params {Object} Object containing animation params
+     * @param queue {Queue} Queue this animation will be added to
+     * @returns {Object} Object containing Animation's params and render function
      */
-    function makeTween(target, params){
-        return function(progress){
+    function makeTween(target, params, queue){
+        if(typeof params === 'object'){
+            var p = {};
 
-            //calculate local progress for current animation
-            var tweenProgress = (progress - params.start) / params.duration;
-
-
-            //if animation is playing
-            if(tweenProgress > 0 && tweenProgress < 1){
-                target.style[params.property] = params.prefix + (params.from + (params.to - params.from) * (tweenProgress)) + params.suffix;
-
-            //if animation is already finished
-            }else if(tweenProgress >= 1){
-                target.style[params.property] = params.prefix + params.to + params.suffix;
-
-            //if animation is not started yet
-            }else if(tweenProgress <= 0){
-                target.style[params.property] = params.prefix + params.from + params.suffix;
+            //process animation params
+            p.property = params.property;
+            p.from = params.from || 0;
+            p.to = params.to || 0;
+            p.prefix = params.prefix || '';
+            p.suffix = params.suffix || '';
+            p.duration = toPercents(params.duration) || 0;
+            p.maxSpeed = parseFloat(params.maxSpeed);
+            //if start is not specified set it to the end of queue
+            if(typeof (p.start = toPercents(params.start)) === 'undefined'){
+                p.start = queue.endTime;
+                queue.endTime += p.duration;
             }
-        };
+            p.queue = queue;
+
+            return {
+                animator: null,
+                sourceParams: params, //remember source params
+                params : p, //processed params
+                render: function(progress){
+
+                    //calculate local progress for current animation
+                    var tweenProgress = (progress - this.params.start) / this.params.duration;
+
+                    //if animation is playing
+                    if (tweenProgress > 0 && tweenProgress < 1){
+                        target.style[this.params.property] = this.params.prefix + (this.params.from + (this.params.to - this.params.from) * (tweenProgress)) + this.params.suffix;
+
+                        //if animation is already finished
+                    }else if (tweenProgress >= 1){
+                        target.style[this.params.property] = this.params.prefix + this.params.to + this.params.suffix;
+
+                        //if animation is not started yet
+                    }else if (tweenProgress <= 0){
+                        target.style[this.params.property] = this.params.prefix + this.params.from + this.params.suffix;
+                    }
+                },
+                recalc: function(){
+                    var p = {};
+
+                    p.from = this.sourceParams.from || 0;
+                    p.to = this.sourceParams.to || 0;
+                    p.prefix = this.sourceParams.prefix || '';
+                    p.suffix = this.sourceParams.suffix || '';
+                    p.duration = toPercents(this.sourceParams.duration || '');
+                    if(typeof (p.start = toPercents(this.sourceParams.start)) === 'undefined'){
+                        p.start = queue.endTime;
+                        queue.endTime += p.duration;
+                    }
+
+                    this.params = p;
+                },
+                /**
+                 * Get intersection of custom numbers range and animation duration range
+                 * @param lastProgress {Number}
+                 * @param progress {Number}
+                 * @returns {{from: Number, to: Number}}
+                 */
+                getIntersection: function(lastProgress, progress){
+                    var start = Math.min(lastProgress, progress),
+                        end = Math.max(lastProgress, progress),
+                        from = this.params.start,
+                        to = this.params.start + this.params.duration,
+                        n1,
+                        n2;
+
+                    if(start <= from && end >= from){
+                        n1 = from;
+                        n2 = Math.min(end, to);
+                    }else if(end >= to && start <= to){
+                        n1 = Math.max(start, from);
+                        n2 = Math.min(end, to);
+                    }else if(start >= from && end <= to){
+                        n1 = start;
+                        n2 = end;
+                    }else return;
+
+                    return (lastProgress < progress ? {from: n1, to: n2} : {from: n2, to: n1});
+                }
+            }
+        }
     }
 
     /**
@@ -115,61 +216,14 @@ function Scrollissimo(callback){
     /**
      * Add animation to the queue
      * @param target {HTMLElement} Target of animation
-     * @param params {Object} Object contains animation params
-     * @returns {Scrolissimo.Queue|undefined}
+     * @param params {Object} Object containing animation params
+     * @returns {Scrollissimo.Queue|undefined}
      */
     S.Queue.prototype.then = function(target, params){
-        var tween;
-        params = params || {};
-
-        params.property = params.property || '';
-        params.prefix =   params.prefix   || '';
-        params.suffix =   params.suffix   || '';
-        params.from =     !isNaN(params.from) && params.from;
-        params.to =       !isNaN(params.to)   && params.to;
-
-        //start parameter might be specified as a string ('100px' or '100%')
-        if(typeof params.start === 'string'){
-
-            //if start parameter specified in pixels
-            if(params.start.substr(-1) === '%'){
-                params.start = parseInt(params.start) / 100;
-
-            //if start parameter specified in percents
-            }else if(params.start.substr(-2) === 'px'){
-                params.start = px2percents(params.start);
-
-            //if unit is not specified unit sets percents by default
-            }else{
-                params.start = px2percents(params.start);
-            }
-
-        //if start parameter is not specified it puts to the end of this animation's queue
-        }else if(isNaN(params.start)){
-            params.start = this.endTime;
-
-            //increase total duration of this animation's queue
-            this.endTime += params.duration;
-        }
-
-        //if duration specified
-        if(typeof params.duration === 'string'){
-
-            //if duration specified in percents
-            if(params.duration.substr(-1) === '%'){
-                params.duration = parseInt(params.duration) / 100;
-            }else if(params.duration.substr(-2) === 'px'){
-                params.duration = px2percents(params.duration);
-            }else{
-                params.duration = px2percents(params.duration);
-            }
-        }else if(isNaN(params.duration)) return;
-
-        //make a render function
-        tween = makeTween(target, params);
+        var tween = makeTween(target, params, this);
 
         //if tween made successfully
-        if(typeof tween === 'function'){
+        if(typeof tween === 'object'){
             this.push(tween);
         }
 
@@ -182,66 +236,105 @@ function Scrollissimo(callback){
      * @namespace Scrollissimo
      */
     (function(S){
-        var scroll = 0,
-            lastScroll = 0,
-            smoothStatus = 'stoped',
-            smoothStart,
-            smoothSpeed;
+        var lastProgress = 0;
 
-        S.Smoother = function(scrollTop){
-            var delta = scrollTop - lastScroll;
+        /**
+         * Render smooth scroll animation
+         * @param progress {Number} Current progress
+         * @param scrollTop {Number} scrollTop value
+         */
+        S.Smoother = function(progress){
+            S.Smoother.smooth(progress);
+        };
 
-            scroll = scrollTop;
+        /**
+         * Animation controlling class
+         * @param tween {Object} Tween object
+         * @constructor
+         */
+        S.Smoother.Animator = function(tween){
+            this.status = 'stopped';
+            this.animateFrom = 0;
+            this.animateTo = 0;
+            this.tween = tween;
+        };
 
-            if(smoothStatus !== 'playing' && lastScroll !== scrollTop){
-                smoothSpeed = 3 * Math.abs(delta) + 1;
-                S.Smoother.run();
+        /**
+         * Smoothing step function
+         */
+        S.Smoother.Animator.prototype.step = function(){
+            var delta = this.animateTo - this.animateFrom,
+                absDelta = Math.abs(delta),
+                maxSpeed = this.tween.params.maxSpeed;
+
+            if(absDelta > maxSpeed){
+                delta = (delta > 0 ? maxSpeed : -maxSpeed);
+                absDelta = Math.abs(delta);
+            }
+
+            this.animateFrom += delta;
+
+            this.tween.render.call(this.tween, this.animateFrom);
+
+            if(absDelta > 0){
+                requestAnimationFrame(this.step.bind(this));
+            }else{
+                this.status = 'stopped';
             }
         };
 
-        S.Smoother.run = (function(delta){
-            smoothStatus = 'playing';
-            requestAnimationFrame((function(time){ this.step(time, true); }).bind(this));
-        }).bind(S.Smoother);
-
-        S.Smoother.step = (function(time, firstTime){
-            var smoothProgress = (firstTime ? (smoothStart = time) && 0 : (time - smoothStart) / smoothSpeed),
-                progress = ((lastScroll + (scroll - lastScroll) * smoothProgress) / (docHeight - windowHeight));
-
-            if(smoothProgress < 1){
-                this.render(progress);
-                requestAnimationFrame(this.step);
-            }else{
-                this.render(progress);
-                smoothStatus = 'stopped';
-                lastScroll = getScrollTop();
+        /**
+         * Run smoothing
+         * @returns {S.Smoother.Animator}
+         */
+        S.Smoother.Animator.prototype.run = function(){
+            this.status = 'playing';
+            if(this.id){
+                cancelAnimationFrame(this.id);
             }
-        }).bind(S.Smoother);
+            this.id = requestAnimationFrame(this.step.bind(this));
+            return this;
+        };
 
-        S.Smoother.render = (function(progress){
+        /**
+         * Smoothly run each tween
+         */
+        S.Smoother.smooth = (function(progress){
+            var self = this;
             smoothQueues.forEach(function(queue){
                 queue.forEach(function(tween){
-                    tween(progress);
+                    var intersection = tween.getIntersection.call(tween, lastProgress, progress);
+
+                    if(intersection && (intersection.from !== intersection.to)){
+                        if((!tween.animator && (tween.animator = new self.Animator(tween))) || tween.animator.status !== 'playing'){
+                            tween.animator.animateTo = intersection.to;
+                            tween.animator.run();
+                        }else{
+                            tween.animator.animateTo = intersection.to;
+                        }
+                    }
                 });
             });
+
+            lastProgress = progress;
         }).bind(S.Smoother);
     })(S);
 
     /**
      * Create new queue and add animation to the beginning
      * @param target {HTMLElement} Target of animation
-     * @param params {Object} Object contains animation's params
+     * @param params {Object} Object containing animation's params
      * @param smooth {Boolean} Add smooth effect to this queue's animations
      * return {Scrollissimo.Queue}
      */
-    S.add = (function(target, params, smooth){
+    S.add = (function(target, params){
         var newQueue;
 
         //create new Queue and add animtion to the beginning
         newQueue = new this.Queue().then(target, params);
 
         //add new queue to all queues
-        (smooth ? smoothQueues : queues).push(newQueue);
+        (params.maxSpeed ? smoothQueues : queues).push(newQueue);
 
         return newQueue;
     }).bind(S);
@@ -254,53 +347,33 @@ function Scrollissimo(callback){
             queue.forEach(function(tween){
 
                 //render current state related to a current scroll progress
-                tween(progress);
+                tween.render(progress);
             });
         });
     }).bind(S);
 
     window.addEventListener('scroll', (function(){
-        var scrollTop = getScrollTop(), //calculate current scroll
-            progress = Math.ceil(scrollTop / (docHeight - windowHeight) * 100) / 100; //calculate current scroll progress
-
-        //render animations with smooth effect
-        this.Smoother(scrollTop);
-
-        //render all other animations
-        this.render(progress);
-
-        //remember current scroll value
-        lastScroll = scrollTop; //TODO: Remove this
+        //TODO: remove this shit
     }).bind(S));
+
+
+    function scrollCatcher(){
+        var scrollTop = getScrollTop(), //calculate current scroll
+            progress = scrollTop / (docHeight - windowHeight); //calculate current scroll progress
+
+        if(scrollTop !== lastScroll){
+            //render animations with smooth effect
+            this.Smoother(progress);
+
+            //render all other animations
+            this.render(progress);
+        }
+        lastScroll = scrollTop;
+        requestAnimationFrame(scrollCatcher.bind(this));
+    }
+
+    requestAnimationFrame(scrollCatcher.bind(S));
 
     (callback || function(){}).call(S);
     return S;
 }
-
-window.onload = function(){
-    //initialize Scrollissimo
-    var scrollissimo = Scrollissimo(function(){
-
-        //create new queue with animation of scroll indicator
-        this.add(document.getElementById('indicator'), {
-            property: 'width', //animate width
-            prefix: '',
-            from: 0, //from 0...
-            to: 100, //...to 100...
-            suffix: '%', //...percents
-            start: '0%', //start at the beginning of page
-            duration: '100%' //finish at the end of page
-        });
-
-        //create new queue with animation of header's background
-        this.add(document.getElementById('header'), {
-            property: 'background-position', //animate background's position
-            prefix: 'right -', //X value is constant and always equal 'right' and minus for Y value
-            from: 0, //from 0...
-            to: 400, //...to 400..
-            suffix: 'px', //..pixels
-            start: '0%', //start at the beginning of page
-            duration: '884px' //finish after 884 pixels
-        }, true);
-    });
-};
